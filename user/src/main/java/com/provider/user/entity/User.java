8eafb461f01a60a138a601299e21746fd419dda7
cyh <756085049@qq.com>
2019-09-27 19:06:10 +0800

    服务注册与服务发现

---
+++ user/src/main/java/com/provider/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.provider.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private Integer age;
+
+    private BigDecimal balance;
+}
