a5ff3977a8742fde3e23a6754499632ccec766c1
cyh <756085049@qq.com>
2019-09-29 16:26:31 +0800

    Feign使用Hystrix

---
+++ movie-feign-hystrix/src/main/java/com/consumer/moviefeignhystrix/controller/MovieController.java
@@ -0,0 +1,23 @@
+package com.consumer.moviefeignhystrix.controller;
+
+import com.consumer.moviefeignhystrix.entity.User;
+import com.consumer.moviefeignhystrix.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/movies")
+@RestController
+public class MovieController {
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable Long id){
+        User user = this.userFeignClient.findById(id);
+        return user;
+    }
+}
