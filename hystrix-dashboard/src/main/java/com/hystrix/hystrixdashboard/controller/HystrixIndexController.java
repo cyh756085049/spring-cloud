ab2b8b0fb0b28d682757e3b0dbe7d103e2379b2a
cyh <756085049@qq.com>
2019-09-30 15:14:30 +0800

    Hystrix Dashboard

---
+++ hystrix-dashboard/src/main/java/com/hystrix/hystrixdashboard/controller/HystrixIndexController.java
@@ -0,0 +1,14 @@
+package com.hystrix.hystrixdashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HystrixIndexController {
+
+    @GetMapping("")
+    public String index() {
+        return "forward:/hystrix";
+    }
+}
