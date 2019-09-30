3aff425e7526ca75bd218a23f85b9d5309750c67
cyh <756085049@qq.com>
2019-09-30 14:51:36 +0800

    feign-hystrix监控

---
+++ movie-feign-hystrix-stream/src/main/java/com/consumer/moviefeignhystrixstream/MovieFeignHystrixStreamApplication.java
@@ -0,0 +1,17 @@
+package com.consumer.moviefeignhystrixstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
+public class MovieFeignHystrixStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFeignHystrixStreamApplication.class, args);
+    }
+
+}
---
+++ movie-feign-hystrix-stream/src/main/java/com/consumer/moviefeignhystrixstream/feign/UserFeignClient.java
@@ -0,0 +1,25 @@
+package com.consumer.moviefeignhystrixstream.feign;
+
+import com.consumer.moviefeignhystrixstream.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+
+@FeignClient(name = "user", fallback = UserFeignClientFallback.class)
+public interface UserFeignClient {
+    @GetMapping("/users/{id}")
+    User findById(@PathVariable("id") Long id);
+}
+
+@Component
+class UserFeignClientFallback implements UserFeignClient {
+
+    @Override
+    public User findById(Long id) {
+        return new User(id, "默认用户", "默认用户", 0, new BigDecimal(1));
+    }
+}
