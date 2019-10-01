ac5bf50b8eda26dd3be2b7951495a8dbf3ac5dc8
cyh <756085049@qq.com>
2019-10-02 00:29:24 +0800

    Zuul-fallback

---
+++ gateway-zuul-fallback/src/main/java/com/gateway/gatewayzuulfallback/GatewayZuulFallbackApplication.java
@@ -0,0 +1,15 @@
+package com.gateway.gatewayzuulfallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayZuulFallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulFallbackApplication.class, args);
+    }
+
+}
